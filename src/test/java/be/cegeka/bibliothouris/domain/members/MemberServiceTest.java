5de62e35214417cbced09c1aec51d4e8c2fcb923
walegria <william.alegriamira@cegeka.com>
2017-01-27 16:40:26 +0100

    Test Exceptions

--- src/test/java/be/cegeka/bibliothouris/domain/members/MemberServiceTest.java
+++ src/test/java/be/cegeka/bibliothouris/domain/members/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package be.cegeka.bibliothouris.domain.members;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,7 +10,9 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,23 +30,42 @@ public class MemberServiceTest {
     @Mock
     private MemberRepository memberRepository;
 
+    private List<Member> members = new ArrayList<>();
+
     @Before
     public void setUp() throws Exception {
-        ReflectionTestUtils.setField(memberService, "counter", new AtomicLong(4l));
+        Member member1 = new Member("10", "William","Alegria", "Henri Liebrechtlaan",39,1090, "Jette");
+        Member member2 = new Member("20", "William2","Alegria", "Henri Liebrechtlaan",39,1090, "Jette");
+        Member member3 = new Member("30", "William","Alegria", "Henri Liebrechtlaan",39,1090, "Jette");
+        members.addAll(Arrays.asList(member1,member2,member3));
     }
 
     @Test
     public void addMember_ShouldCallUserRepository() throws Exception {
-        memberService.addMember("Seppe");
+        memberService.addMember(members.get(0));
+        verify(memberRepository).addMember(members.get(0));
+    }
 
-        verify(memberRepository).addMember(new Member(5l, "Seppe"));
+    @Test
+    public void addMember_ValidateExistingUser() throws Exception{
+        Member member4 = new Member("30", "Willy","Aly", "Henri",39,1090, "Jette");
+//        Member member5 = new Member("", "William","", "Henri Liebrechtlaan",39,1090, "");
+        when(memberRepository.getAllMembers()).thenReturn(members);
+        Assertions.assertThat(memberService.addMember(member4)).;
+        /*
+         if (member.getInsz().isEmpty()||member.getLastName().isEmpty()||member.getCity().isEmpty()) {
+            throw new MemberValidationException("Needs to be filled in");
+        }
+        else if (memberRepository.getAllMembers().contains(member)) {
+            throw new MemberValidationException("That member already exists");
+        }
+        else {
+            memberRepository.addMember(member);
+         */
     }
 
     @Test
     public void getAllMembers() throws Exception {
-        Member member1 = new Member(1l, "Seppe");
-        Member member2 = new Member(2l, "Sanne");
-        Member member3 = new Member(3l, "Xan");
 
         when(memberRepository.getAllMembers()).thenReturn(Arrays.asList(member1, member2));
 
